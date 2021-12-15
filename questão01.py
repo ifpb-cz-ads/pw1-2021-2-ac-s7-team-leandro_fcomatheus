ba7721c2e6fe38812ff87770f077edbc101d138b
Lacerda234 <70702222+Lacerda234@users.noreply.github.com>
2021-12-15 14:59:46 -0300

    adicionando questão 1

---
+++ questão01.py
@@ -0,0 +1,13 @@
+notas=[0,0,0,0,0,0,0]
+soma=0
+x=0
+while x<7:
+    notas[x]=float(input('Nota %d: '% x))
+    soma += notas[x]
+    x+=1
+x=0
+print('As notas informadas foram: ')
+while x<7:
+    print('Nota %d: %6.2f' %(x,notas[x]))
+    x+=1
+print('Média: %5.2f' %(soma/x))
